f54092c128b1bf6ab1a64fb2eb8caee0c8b9bd81
Lomnyak Nabaala <lomnyaknab@gmail.com>
2025-09-24 13:03:18 +0300

    committed

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,18 +1,9 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
+const Home = () => {
+       return (
+         <div>
+           <h1>Welcome to Splash App</h1>
+         </div>
+       )
+     }
 
-const Home: NextPage = () => {
-  return (
-    <div className="container mx-auto py-20">
-      <Head>
-        <title>Splash App</title>
-        <meta name="description" content="Your one-stop platform for all your needs." />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1 className="text-4xl font-bold text-center">Welcome to Splash App</h1>
-      <p className="text-center mt-4">This is the homepage content.</p>
-    </div>
-  );
-};
-
-export default Home;
+     export default Home;
